70cab80f6f8eb11087d573adc44dd8f45f510113
hzelfy <hamzanuaten@gmail.com>
2025-09-10 16:07:18 +0100

    Add Netlify Functions and redirects for client-side routing

---
+++ netlify/functions/submit.js
@@ -0,0 +1,8 @@
+exports.handler = async (event) => {
+  const data = JSON.parse(event.body);
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ message: `Received data: ${data.name}` }),
+  };
+};
